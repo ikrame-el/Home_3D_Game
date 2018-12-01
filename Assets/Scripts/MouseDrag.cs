ece494d337b9cec98475a4e3087c80e3377d19b8
ikrame el arabi <ikrame-el-arabi@hotmail.com>
2018-12-01 18:30:48 +0000

    MouseDrag.cs édité en ligne via Bitbucket

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -6,6 +6,7 @@ public class MouseDrag : MonoBehaviour {
 
     void OnMouseDrag()
     {
+    //coucou j'ai ete modifier sur bitbucket
         Debug.Log("dragDragDrag");
         Vector3 mousePosition = new Vector3(Input.mousePosition.x,0 ,Input.mousePosition.y);
         transform.position = mousePosition/100;
